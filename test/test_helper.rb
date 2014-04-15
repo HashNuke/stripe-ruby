62313aee9fbe95758194d95251b264802ba6467f
Akash Manohar J <akash@akash.im>
2014-04-15 19:10:05 +0530

    Remove monkey patch to Stripe module in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,24 +4,6 @@ require 'mocha/setup'
 require 'stringio'
 require 'shoulda'
 
-#monkeypatch request methods
-module Stripe
-  @mock_rest_client = nil
-
-  def self.mock_rest_client=(mock_client)
-    @mock_rest_client = mock_client
-  end
-
-  def self.execute_request(opts)
-    get_params = (opts[:headers] || {})[:params]
-    post_params = opts[:payload]
-    case opts[:method]
-    when :get then @mock_rest_client.get opts[:url], get_params, post_params
-    when :post then @mock_rest_client.post opts[:url], get_params, post_params
-    when :delete then @mock_rest_client.delete opts[:url], get_params, post_params
-    end
-  end
-end
 
 def test_response(body, code=200)
   # When an exception is raised, restclient clobbers method_missing.  Hence we
@@ -378,13 +360,10 @@ class Test::Unit::TestCase
   include Mocha
 
   setup do
-    @mock = mock
-    Stripe.mock_rest_client = @mock
     Stripe.api_key = ENV["STRIPE_TEST_API_KEY"]
   end
 
   teardown do
-    Stripe.mock_rest_client = nil
     Stripe.api_key=nil
   end
 end
