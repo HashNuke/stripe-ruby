e844ecb1127ebe6343d59bfdd9fc33ca1cb7d90e
Akash Manohar J <akash@akash.im>
2014-04-15 20:18:47 +0530

    Use test account for Account tests

--- test/stripe/account_test.rb
+++ test/stripe/account_test.rb
@@ -2,13 +2,13 @@ require File.expand_path('../../test_helper', __FILE__)
 
 module Stripe
   class AccountTest < Test::Unit::TestCase
+
     should "account should be retrievable" do
-      resp = {:email => "test+bindings@stripe.com", :charge_enabled => false, :details_submitted => false}
-      @mock.expects(:get).once.returns(test_response(resp))
       a = Stripe::Account.retrieve
-      assert_equal "test+bindings@stripe.com", a.email
-      assert !a.charge_enabled
-      assert !a.details_submitted
+      assert_true a.email.is_a?(String)
+      assert_true [false, true].include?(a.charge_enabled)
+      assert_true [false, true].include?(a.details_submitted)
     end
+
   end
 end
